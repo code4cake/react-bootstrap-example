7cec1d1d5aa86701b144323fdce7b9da7b9cbf4e
DS <ds@Mac-mini-de-DS.local>
2020-06-02 19:53:16 +0200

    Add navbar and some styling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-// import logo from './logo.svg';
+import PropTypes from 'prop-types';
+
+import {
+ Button,
+ Card,
+ Col,
+ Container,
+ Navbar,
+ Nav,
+ NavDropdown,
+ Row
+} from 'react-bootstrap';
+
 import './App.scss';
-
-import { Button, Card, Container, Row, Col } from 'react-bootstrap';
-
 
 const renderBtnSet = () => (
   <>
@@ -11,11 +20,11 @@ const renderBtnSet = () => (
     <Button size="md" variant="custom">I am a Custom Btn </Button> {` `}
     <Button size="sm" variant="link">I am a Link</Button> {` `}
     <Button size="sm" variant="lightGreen" disabled>I am a Disabled Btn</Button> {` `}
- </>
+  </>
 )
 
 const renderCard = () => (
-<Card style={{ width: '18rem' }}>
+<Card style={{ width: '15rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" /> 
   <Card.Body>
     <Card.Title>I am a Nice Card</Card.Title>
@@ -27,34 +36,54 @@ const renderCard = () => (
 </Card>
 )
 
-
 const renderNavbar = () => (
-  <div>Hello</div>
+<Navbar bg="navbarColor" expand="lg">
+  <Navbar.Brand href="#home">Stockly</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Landlord</Nav.Link>
+      <Nav.Link href="#tenant">Tenant</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 )
 
 function App() {
   return (
     <div className="App">
 
-      <Container>
-      <h6>Row of Btns</h6>
-        <section>
+      <header>
+        {renderNavbar()}
+      </header>
+
+      <section>
+        <Container fluid>
+          <h6>Row of Btns</h6>
           {renderBtnSet()}
-        </section>
-      </Container >
+        </Container>
+      </section>
 
        <section>
           <Container>
-          <h6>Row of Cards</h6>
-            <Row xs={1} md={4} lg={6}>
-              <Col>
-                {renderCard()}
-                {renderCard()}
-                {renderCard()}
-              </Col>
+            <h6>Row of Cards</h6>
+            <Row sm={1} md={4} lg={5}>
+              <Col lg="auto">{renderCard()}</Col>
+              <Col lg="auto">{renderCard()}</Col>
+              <Col lg="auto">{renderCard()}</Col>
+              <Col lg="auto">{renderCard()}</Col>
+              <Col lg="auto">{renderCard()}</Col>
             </Row>
           </Container>
-       </section>
+       </section>       
     </div>
   );
 }
